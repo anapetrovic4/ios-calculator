d557df895b0c09769b1392c97811952e228b5d0e
Ana Petrovic <petrovic.bs.ana@gmail.com>
2023-12-27 12:20:22 +0100

    refactoring the code

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 //Global variables
 let firstNumber = '';
-let currentInput = '0'; //avoid parsing issues
+let currentInput = '0'; //Avoid parsing issues
 let operator = '';
 let resultValue = 0;
 
+//Selectors 
 const numbers = document.querySelectorAll('.number');
 const operations = document.querySelectorAll('.operation');
 const equal = document.querySelector('.equal');
@@ -11,27 +12,7 @@ const decimal = document.querySelector('.decimal');
 const reset = document.querySelector('.reset');
 const result = document.querySelector('.result');
 
-//Function to handle number clicks
-//the function ensures that the currentInput is always a number
-function handleNumberClick(number){
-
-    //add . to the number
-    const hasDecimal = currentInput.toString().includes('.');
-
-    if(!hasDecimal || operator !== ''){
-        currentInput = parseFloat(currentInput.toString() + number);
-        console.log(typeof(currentInput));
-    } else {
-        const decimalPart = parseFloat(number.toString().replace(/^0+/, ''));
-        currentInput += decimalPart;
-        currentInput = parseFloat(currentInput);
-        console.log(typeof(currentInput));
-    }
-    updateDisplay();
-}
-
-//Event listeners for numbers
-//forEach is used for readability
+//Event listener for numbers
 numbers.forEach((numbers) => {
     numbers.addEventListener('click', () => {
         handleNumberClick(numbers.textContent);
@@ -46,15 +27,50 @@ decimal.addEventListener('click', function() {
     }
 });
 
-//Event listeners for operations
+//Event listener for operations
 operations.forEach((operation) => {
     operation.addEventListener('click', () => {
         handleOperationClick(operation.textContent);
-        console.log('operation clicked');
     });
 });
 
-// Function to handle operation clicks
+// Event listener for the equal button
+equal.addEventListener('click', function () {
+    if (operator !== '') {
+        performOperation();
+        operator = '';
+    }
+    updateDisplay();
+});
+
+//Event listener to handle reset button click
+reset.addEventListener('click', function() {
+    firstNumber = '';
+    currentInput = '0';
+    operator = '';
+    result.textContent = currentInput;
+    updateDisplay();
+});
+
+//Function to handle number clicks
+//The function ensures that the currentInput is always a number
+function handleNumberClick(number){
+
+    //Add . to the number
+    const hasDecimal = currentInput.toString().includes('.');
+
+    if(!hasDecimal || operator !== ''){
+        currentInput = parseFloat(currentInput.toString() + number);
+    } else {
+        const decimalPart = parseFloat(number.toString().replace(/^0+/, ''));
+        currentInput += decimalPart;
+        currentInput = parseFloat(currentInput);
+    }
+    updateDisplay();
+}
+
+
+//Function to handle operation clicks
 function handleOperationClick(clickedOperator) {
     if (operator !== '') {
         performOperation();
@@ -65,7 +81,7 @@ function handleOperationClick(clickedOperator) {
     updateDisplay();
 }
 
-// Function to perform operations
+//Function to perform operations
 function performOperation() {
     const num1 = parseFloat(firstNumber);
     const num2 = parseFloat(currentInput);
@@ -86,27 +102,8 @@ function performOperation() {
         default:
             break;
     }
-
-    operator = '';
 }
 
-// Event listener for the equal button
-equal.addEventListener('click', function () {
-    if (operator !== '') {
-        performOperation();
-        operator = '';
-    }
-    updateDisplay();
-});
-
-//Function to handle reset button click
-reset.addEventListener('click', function() {
-    firstNumber = '';
-    currentInput = '0';
-    operator = '';
-    result.textContent = currentInput;
-    updateDisplay();
-});
 
 //Function to update display
 function updateDisplay() {
